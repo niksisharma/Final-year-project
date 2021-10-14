0a2a58b65fcbbe952873128623d9184cfac8d00d
Nikita Sharma <51536421+niksisharma@users.noreply.github.com>
2021-10-14 19:41:04 +0530

    Update freq_mod.m

--- MATLAB/Assignment 2/Frequency Modulation/freq_mod.m
+++ MATLAB/Assignment 2/Frequency Modulation/freq_mod.m
@@ -1,11 +1,13 @@
 clear all;
 close all;
-fc=1500; %Carrier Frequency
-fm=250; %Modulating Frequency
-m=2; %Modulating Index
+Am=1;
+kf=500; %Frequency Deviation
+fc=1000; %Carrier Frequency
+fm=200; %Modulating Frequency
+m=kf*Am/fm; %Modulating Index
 n=0:0.0001:.1; %Time index
-c=sin(2*pi*fc*n); %Carrier Signal
-M=sin(2*pi*fm*n); %Modulating Signal
+c=cos(2*pi*fc*n); %Carrier Signal
+M=cos(2*pi*fm*n); %Modulating Signal
 subplot(511);
 plot (n,c);
 ylabel('Amplitude');
@@ -16,7 +18,7 @@ plot (n,M);
 ylabel('Amplitude');
 xlabel('Time index');
 title('Modulating Signal');
-y=sin(2*pi*fc*n+(m.*sin(2*pi*fm*n))); %Frequency Modulation
+y=cos(2*pi*fc*n+(m.*sin(2*pi*fm*n))); %Frequency Modulation
 subplot(513);
 plot (n,y);
 ylabel('Amplitude');
@@ -25,14 +27,14 @@ title('Frequency Modulated signal');
 Fs=4000;
 N=4096;
 df=Fs/N;
-k= (-Fs/2:df:Fs/2-df)*2*pi/Fs;
-S=fftshift(fft(y,N)); %Discrete Fourier Transform
+k=-Fs/2:df:Fs/2-df;
+Y=(fft(y,N)); %Discrete Fourier Transform
 subplot(514);
-plot(k,abs(S));
+plot(k,abs(Y));
 title('Magnitude of Discrete Fourier Transform');
-xlabel('Frequency (x pi, radians per second)');
-p = unwrap(angle(S));
+xlabel('Frequency');
+p = unwrap(angle(Y));
 subplot(515);
 plot(k,p);
 title('Phase of Discrete Fourier Transform');
-xlabel('Frequency (x pi, radians per second)');
+xlabel('Frequency');
