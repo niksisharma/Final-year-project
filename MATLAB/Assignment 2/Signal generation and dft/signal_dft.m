f44a7d2ebc5827b85bc886f7cb2d56c618d8c908
rosecherian99 <56474541+rosecherian99@users.noreply.github.com>
2021-10-16 16:58:16 +0530

    Update signal_dft.m

--- MATLAB/Assignment 2/Signal generation and dft/signal_dft.m
+++ MATLAB/Assignment 2/Signal generation and dft/signal_dft.m
@@ -5,21 +5,42 @@ dt = 1/Fs;
 t = (0:dt:0.002-dt);
 Fm = 5000;
 y = cos(2*pi*Fm*t);
-subplot(3,1,1);
+subplot(3,2,1);
 plot(t, y);
 title('Time-Domain signal')
 ylabel('Amplitude');
 xlabel('Time index');
 N=16000;
 df = Fs/N;
-k= (-Fs/2:df:Fs/2-df)*2*pi/Fs;
-S=fftshift(fft(y,N)); %Discrete Fourier Trasnform
-subplot(3,1,2);
+k= (-Fs/2:df:Fs/2-df);
+S=(fft(y,N)); %Discrete Fourier Trasnform
+subplot(3,2,3);
 plot(k,abs(S));
 title('Magnitude of Discrete Fourier Transform');
-xlabel('Frequency (x pi, radians per second)');
+xlabel('Frequency');
 p = unwrap(angle(S));
-subplot(3,1,3);
+subplot(3,2,5);
 plot(k,p);
 title('Phase of Discrete Fourier Transform');
-xlabel('Frequency (x pi, radians per second)');
+xlabel('Frequency');
+snr=5; %Signal to noise ratio
+z = awgn(y,snr,'measured'); %Adding noise(AWGN)
+Z=(fft(z,N)); %Discrete Fourier Transform on time domain signal
+subplot(3,2,4);
+plot(k,abs(S),'r');
+hold on;
+plot(k,abs(Z),'b');
+title('Magnitude of Discrete Fourier Transform (with noise)');
+xlabel('Frequency');
+grid on;
+hold off;
+legend('Original Signal','Signal with AWGN');
+subplot(3,2,6);
+plot(k,unwrap(angle(S)),'r');
+hold on;
+plot(k,unwrap(angle(Z)),'b');
+title('Phase of Discrete Fourier Transform (with noise)');
+xlabel('Frequency');
+grid on;
+hold off;
+legend('Original Signal','Signal with AWGN');
